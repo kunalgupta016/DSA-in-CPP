4f0dd5b52dce21ce4b587d8945059b0c6d15629e
kunalgupta016 <kunalgupta55005@gmail.com>
2024-11-09 01:43:55 +0530

    add files:

--- stack/redundant.cpp
+++ stack/redundant.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    string str= "((a+b))";
+    string str= "(a+b)";
     stack<char> st;
     for(int i = 0 ; i < str.length() ; i++){
         char ch  = str[i];
